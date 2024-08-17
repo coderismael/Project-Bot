9bd210a730eceb7ff06e5842837c926bb905346c
Coder Ismael <124251792+coderismael@users.noreply.github.com>
2024-08-17 15:46:39 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -82,3 +82,23 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+ const btn = document.getElementById('button');
+
+document.getElementById('form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_8x1ggjw';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
